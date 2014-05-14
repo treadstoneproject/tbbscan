112fdc56cd3685accd46977bbf3eaa03c32be640
Chatsiri <chatsiri@chatsiri.com>
2014-05-14 13:16:31 +0700

    Add task example.

---
+++ task/file_pe_controller_task.h
@@ -0,0 +1,13 @@
+#ifndef TBBSCAN_FILE_PE_CONTROLLER_TASK_HPP
+#define TBBSCAN_FILE_PE_CONTROLLER_TASK_HPP
+
+#include "task/filebase_task.h"
+
+template<typename use_tbbmalloc, typename mappfile_structure>
+class file_pe_controller_task : public filebase_task<use_tbbmalloc, mappfile_structure>{
+
+
+
+};
+
+#endif // !TBBSCAN_FILE_PE_TASK_CONTROLLER_HPP
