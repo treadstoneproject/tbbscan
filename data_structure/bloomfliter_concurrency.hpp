b76556b626d0cdc37bbd8dc9b804fa8669cafdce
Chatsiri Rattana <chatsiri@chatsiri.com>
2014-07-11 06:20:23 +0700

    [TASK]: Bloom filter support serial bloom function. ,#00003
    Detail:
    * Bloom filter support serial bloom function. That commits initial version for tbb in new branch.
    * Bloom filter not use MD5 hash for hash string.
    
    ReviewedBy: R.Chatsiri
    AutomergeTo:
    
    Resolves: #
    Documentation: #
    Release:
    
    	new file:   data_structure/bloomfliter_concurrency.cpp
    	modified:   data_structure/bloomfliter_concurrency.hpp

--- data_structure/bloomfliter_concurrency.hpp
+++ data_structure/bloomfliter_concurrency.hpp
@@ -20,24 +20,75 @@
 /*  Titles			                                          Authors	         Date
  * -Bloom filter run on load balance tbb and bloom filter
  *                                                        R.Chatsiri       02/06/2014
- * -Dynamic Bloom filter 
+ * -Bloom filter
  *                                                        R.Chatsiri
  */
+#include "utils/hash.hpp"
+#include "utils/md5_hash.hpp"
+#include "utils/filter_policy.hpp"
+
+namespace concurrency
+{
 
 
-template<typename Symbol, bool AllocatorMemType>
-class bloomfilter_concurrency{
+   	/* 
+    static uint32_t bloom_hash(const utils::slice& key)
+    {
+    return utils::convert::md5_hash(key.data());
+    }
+    */
+		
+    static uint32_t bloom_hash(const utils::slice& key)
+    {
+        return utils::hash(key.data(), key.size(), 0xbc9f1d34);
+    }
 
-private:
-		intitial_
+    class bloom_filter_policy : public utils::filter_policy
+    {
+        private:
+            size_t bits_per_key_;
+            size_t k_;
+            uint32_t *hash_func_;
 
+            void initialize() {
+                // We intentionally round down to reduce probing cost a little bit
+                k_ = static_cast<size_t>(bits_per_key_ * 0.69);  // 0.69 =~ ln(2)
 
+                if (k_ < 1) k_ = 1;
 
-public:
-		size_t bit_per_key_;
-		size_t key_;
-};
+                if (k_ > 30) k_ = 30;
+            }
 
+        public:
+            /*
+            explicit bloom_filter_policy(int bits_per_key,
+            uint32_t (*hash_func)(const utils::slice& key))
+            : bits_per_key_(bits_per_key), hash_func_(hash_func) {
+            initialize();
+            }
+            */
+
+            explicit bloom_filter_policy(int bits_per_key, const utils::slice& key)
+                : bits_per_key_(bits_per_key) {
+                *hash_func_ = bloom_hash(key);
+                initialize();
+            }
+
+            explicit bloom_filter_policy(int bits_per_key)
+                : bits_per_key_(bits_per_key) {
+                //hash_func_ bloom_hash;
+                initialize();
+            }
+
+            virtual const char *name() const {
+                return "tbbscan.buildbloomfiltercuda";
+            }
+
+            virtual void create_filter(const utils::slice *keys, int n, std::string *dst) const;
+            virtual bool key_may_match(const utils::slice& key, const utils::slice& bloom_filter) const;
+    };
+
+}
 
 
 #endif /* CONCURRENCY_BLOOMFILTER_CONCURRENCY_HPP */
