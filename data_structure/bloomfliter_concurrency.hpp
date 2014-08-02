92aff0524d5b88425d2cb33de4182d36741e6c4c
Chatsiri Rattana <chatsiri@chatsiri.com>
2014-08-03 02:38:07 +0700

    [BUGFIX]: Changed License name,#00003
    Detail:
    
    ReviewedBy: R.Chatsiri
    AutomergeTo:
    
    Resolves: #
    Documentation: #
    Release:
    
    	modified:   data_structure/actire_concurrency.cpp
    	modified:   data_structure/actire_concurrency.hpp
    	modified:   data_structure/bloomfliter_concurrency.hpp

--- data_structure/bloomfliter_concurrency.hpp
+++ data_structure/bloomfliter_concurrency.hpp
@@ -2,7 +2,7 @@
 #define CONCURRENCY_BLOOMFILTER_CONCURRENCY_HPP
 
 /*
-* Copyright 2014 MTSec, Inc.
+* Copyright 2014 Chatsiri Rattana.
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
